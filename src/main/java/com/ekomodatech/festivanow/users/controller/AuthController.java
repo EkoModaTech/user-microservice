5323b7d446b7f8ad39e6f51185ad360cc3f377e8
Zayver <zayber.re@gmail.com>
2023-10-10 22:11:59 -0500

    Add validation

--- src/main/java/com/ekomodatech/festivanow/users/controller/AuthController.java
+++ src/main/java/com/ekomodatech/festivanow/users/controller/AuthController.java
@@ -3,30 +3,33 @@ package com.ekomodatech.festivanow.users.controller;
 import com.ekomodatech.festivanow.users.entity.LoginRequest;
 import com.ekomodatech.festivanow.users.entity.RefreshTokenRequest;
 import com.ekomodatech.festivanow.users.service.OIDCService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
+@Validated
 public class AuthController {
     @Autowired
     private OIDCService oidcService;
 
     @PostMapping("/login")
-    public Map<String, String> login(@RequestBody LoginRequest userLogin){
+    public Map<String, String> login(@Valid @RequestBody LoginRequest userLogin){
         return oidcService.login(userLogin);
     }
 
     @PostMapping("/refresh")
-    public Map<String, String> refresh(@RequestBody RefreshTokenRequest token){
+    public Map<String, String> refresh(@Valid @RequestBody RefreshTokenRequest token){
         return oidcService.refreshToken(token.getRefreshToken());
     }
 
 
     @PostMapping("/logout")
-    public Map<String, String> logout(@RequestBody RefreshTokenRequest userLogout, @RequestHeader("Authorization") String token){
+    public Map<String, String> logout(@Valid @RequestBody RefreshTokenRequest userLogout){
         return oidcService.logout(userLogout);
     }
 }
