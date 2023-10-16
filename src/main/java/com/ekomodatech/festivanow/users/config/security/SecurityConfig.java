4d134605734b5c5b10d7662b4e0c8e6a28e5a9a6
Zayver <zayber.re@gmail.com>
2023-10-16 16:01:13 -0500

    LastTest

--- src/main/java/com/ekomodatech/festivanow/users/config/security/SecurityConfig.java
+++ src/main/java/com/ekomodatech/festivanow/users/config/security/SecurityConfig.java
@@ -1,35 +1,31 @@
 package com.ekomodatech.festivanow.users.config.security;
 
+import com.ekomodatech.festivanow.users.config.oidc.OIDCConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private OIDCConfig issuer;
+
     @Bean
-    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.oauth2ResourceServer(resourceServer -> resourceServer
-                .jwt(Customizer.withDefaults())
-        );
-
-        return http.build();
-    }
-
-    @Bean
-    @Order(1)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .oauth2ResourceServer(oAuth2 -> oAuth2
+                        .jwt(it -> it.decoder(JwtDecoders.fromIssuerLocation(issuer.getIssuer_url()))))
                 .build();
     }
 }
