6257e7ff8aa3be375c2e2d9b81b4103a9e2a0316
Zayver <zayber.re@gmail.com>
2023-11-02 23:26:00 -0500

    Added JwtAuthenticationConverter

--- src/main/java/com/ekomodatech/festivanow/users/config/security/SecurityConfig.java
+++ src/main/java/com/ekomodatech/festivanow/users/config/security/SecurityConfig.java
@@ -1,15 +1,27 @@
 package com.ekomodatech.festivanow.users.config.security;
 
 import com.ekomodatech.festivanow.users.config.oidc.OIDCConfig;
+import com.nimbusds.jose.shaded.gson.internal.LinkedTreeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -28,5 +40,23 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oAuth2 -> oAuth2
                         .jwt(it -> it.decoder(JwtDecoders.fromIssuerLocation(issuer.getIssuer_url()))))
                 .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+            Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+            List<String> roles = (List<String>) realmAccess.get("roles");
+
+            return roles.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        };
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
     }
 }
