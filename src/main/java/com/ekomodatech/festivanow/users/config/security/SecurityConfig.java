22b639c82598ff4b6d6d04cafba50146fe081707
Zayver <zayber.re@gmail.com>
2023-11-02 22:44:31 -0500

    Add @EnableMethodSecurity annotation on security config

--- src/main/java/com/ekomodatech/festivanow/users/config/security/SecurityConfig.java
+++ src/main/java/com/ekomodatech/festivanow/users/config/security/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.ekomodatech.festivanow.users.config.security;
 
 import com.ekomodatech.festivanow.users.config.oidc.OIDCConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
